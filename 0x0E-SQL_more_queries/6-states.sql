71c5d5c3d1004d33bd9bbbe5b80c00252c275a3f
themrsgod <themrsgod@gmail.com>
2023-05-17 14:36:04 -0700

    created table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- script that creates the database hbtn_0d_usa with table states with id INT unique, auto generated, can’t be null and is a primary key, name VARCHAR(256) can’t be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (PRIMARY KEY (id), id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
+
