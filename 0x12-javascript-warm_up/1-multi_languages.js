c9147538fb81969312cd77bffb9e72db3e3a6d03
Vivian Heavenian Dzrekey <themrsgod@gmail.com>
2023-06-12 21:27:45 +0000

    wrote 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
-#!/usr/bin/node
+i#!/usr/bin/node
 console.log('C is fun');
 console.log('Python is cool');
-let myVar = 'JavaScript is amazing';
-console.log(myVar);
+console.log('JavaScript is amazing');
